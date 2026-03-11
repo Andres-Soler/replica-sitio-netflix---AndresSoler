9fcb49d6b8015ad789a7ff69c6c2fe07cb5f686f
sambiux <sddaza.9@gmail.com>
2026-03-11 22:13:54 +0000

    Agregando componente que maneja la informacion adicional de la pagina(footer)

--- proyecto/src/App.jsx
+++ proyecto/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import DavidMartinez from './DavidMartinez'
+import NetflixFooter from './footerNetflix'
 
 function App() {
   return (
     <>
       <DavidMartinez />
+      <NetflixFooter/>
     </>
   )
 }
